26cdf1417965ab33cb9341d666685b664523ba13
oXm1N <oxminka@gmail.com>
2021-08-14 16:25:49 +0300

    transpher

--- js/main.js
+++ js/main.js
@@ -1,26 +1,47 @@
+// swiper
+
 const swiper = new Swiper('.swiper-container', {
-    centeredSlides: true,
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  });
+  centeredSlides: true,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});
 
-
-var navButton = document.querySelector("#nav-button");
-var navWrapper = document.querySelector("#nav-wrapper");
-var headerSlider = document.querySelector("header-slider")
+// burger-menu
 
 var body = document.querySelector('body');
-
+var navButton = document.getElementById("nav-button");
+var bgBurger = document.querySelector(".bg-burger");
+var headerNavbar = document.querySelector(".navbar-wrapper");
 
 document.querySelector("#nav-button").addEventListener('click', function(menuOpen){
-  navWrapper.style.display = 'block';
+  bgBurger.style.display = 'block';
+  headerNavbar.style.display = 'block';
   navButton.style.display = 'none';
   body.style.overflow = 'hidden';
 });
 
-document.querySelector("close-burger").addEventListener('click', function(menuClose){
-  navWrapper.style.display = 'none';
+document.getElementById("close-burger").addEventListener('click', function(menuClose){
+  bgBurger.style.display = 'none';
+  headerNavbar.style.display = 'none';
   navButton.style.display = 'block';
   body.style.overflow = 'visible';
 });
+
+
+// button show more / show less
+
+var status = "less";
+function toggleText(){
+    if (status == "less") {
+        document.getElementBycl("products-text").style.display= "block";
+        document.getElementById("product-button").innerText = "Read Less";
+        status = "more";
+    } 
+    else if (status == "more") {
+        document.getElementById("products-text").style.display= "block";
+        document.getElementById("product-button").innerText = "Read More";
+        status = "less"
+    }
+}
+
