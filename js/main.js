3f6cbb70bd44ca5d0b4b3461ce6c82b75d475a7c
oXm1N <oxminka@gmail.com>
2021-08-16 13:17:14 +0300

    transpher
    1

--- js/main.js
+++ js/main.js
@@ -1,9 +1,20 @@
 // swiper
 
-const swiper = new Swiper('.swiper-container', {
+const firstSwiper = new Swiper('.swiper-header', {
   centeredSlides: true,
+  autoplay: {
+    delay: 5000,
+  },
   pagination: {
     el: '.swiper-pagination',
+  },
+});
+
+const secondSwiper = new Swiper('.swiper-products', {
+  centeredSlides: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
   },
 });
 
@@ -28,20 +39,48 @@ document.getElementById("close-burger").addEventListener('click', function(menuC
   body.style.overflow = 'visible';
 });
 
-
-// button show more / show less
+// read more/less button
 
 var status = "less";
 function toggleText(){
-    if (status == "less") {
-        document.getElementBycl("products-text").style.display= "block";
-        document.getElementById("product-button").innerText = "Read Less";
-        status = "more";
-    } 
-    else if (status == "more") {
-        document.getElementById("products-text").style.display= "block";
-        document.getElementById("product-button").innerText = "Read More";
-        status = "less"
-    }
-}
+  if (status == "less") {
+    document.getElementById("products-text").style.display="block";
+    document.getElementById("product-button").innerText = "Read Less";
+    document.getElementById("product-button").style.backgroundImage = "url('../img/btn-top.svg')";
+    status = "more";
+  } else if (status == "more") {
+    document.getElementById("products-text").style.display="none";
+    document.getElementById("product-button").innerText = "Read More";
+    document.getElementById("product-button").style.backgroundImage = "url('../img/btn-vector.svg')";
+    status = "less"
+  }
+};
 
+// get products(include jquery)
+
+$('document').ready(function(){
+  loadItems();
+});
+
+function loadItems() {
+$.getJSON('js/products.json', function (data) {
+  console.log(data);
+    let out = '';
+    for (let key in data){
+      out+='<div class="swiper-slide product-slide">';
+
+      out+='<img src="'+data[key]['image']+'">';
+      out+='<span class="product-item__title">'+data[key]['title']+'</span>';
+
+      out+='<div class="product-item__price">';
+      out+='<span class="product-item__price-idk">'+'םיעבצ4'+'</span>';
+      out+='<span class="product-item__price-text">'+'₪'+data[key]['price']+'</span>';
+      out+='</div>';
+
+      out+='<a href="'+data[key]['link']+'" class="product-item__button">'+'סקירה מהירה'+'</a>';
+
+      out+='</div>';
+  }
+$('#items').html(out);
+})
+}
